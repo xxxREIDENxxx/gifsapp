900812fff7d9e8399420dbcce4b9ec6100c24bf3
Arley <reiden-3@hotmail.com>
2022-08-09 21:02:43 -0500

    primer commit de app imagenes

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams} from '@angular/common/http';
+import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private apiKey = 'nY9kITPGm4d1xX9sDLS0XAN2gFEsGYj8'
+  private servicioUrl = 'https://api.giphy.com/v1/gifs';
+  private _historial: string[] = [];
+  //TODO: ccambar any tipo correspondiente
+  public resultados: Gif[] = []; 
+
+
+  get historial(){
+    return [...this._historial];
+  }
+ 
+  constructor (private http: HttpClient ){
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+    // if(localStorage.getItem("historial")) {
+    //   this._historial = JSON.parse(localStorage.getItem("historial")!);
+    // }
+  }
+
+  buscarGifs(query: string = '') {
+
+    query = query.trim().toLowerCase();
+    
+    if(!this._historial.includes(query)){
+      this._historial.unshift(query);  
+      this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial) );
+     
+    }
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', query);
+    
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`,{params})
+    .subscribe((resp) => {
+      //onsole.log(resp.data);
+      this.resultados = resp.data;
+      localStorage.setItem('resultados', JSON.stringify(this.resultados));
+    })
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=nY9kITPGm4d1xX9sDLS0XAN2gFEsGYj8&q=dbz&limit=10')
+    // .then(resp => {
+    //   resp.json().then(data => {
+    //     console.log(data);
+    //   })
+    // })
+  }
+}
---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styles: [
+  ]
+})
+export class SidebarComponent {
+
+  get historial(){
+    return this.gifsService.historial;
+  }
+
+  constructor(private gifsService: GifsService) { }
+
+  buscar(termino: string){
+    this.gifsService.buscarGifs(termino);
+    console.log(termino);
+  }
+}
